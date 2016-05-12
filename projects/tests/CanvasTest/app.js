fce3b5f9ca1363858a9459190533af92e4d1d030
DazBoot <Lekgold@gmail.com>
2016-05-12 14:07:47 -0400

    Adding some javascript tests

---
+++ projects/tests/CanvasTest/app.js
@@ -0,0 +1,64 @@
+function onLoad( )
+{
+	maxWidth = 720;
+	maxHeight = 400;
+
+	console.log( "PAGE LOADED!" );
+	updateContentDivSize();
+
+	addContentListners();
+}
+
+function addContentListners( )
+{
+	$( blueButton ).click( onBlueContentClick );
+	$( greenButton ).click( onGreenContentClick );
+	$( yellowButton ).click( onYellowContentClick );
+	$( redButton ).click( onRedContentClick );
+}
+
+function onResize( )
+{
+	updateContentDivSize();
+}
+
+function onBlueContentClick( )
+{
+	focusContent.style.backgroundColor = "#00BFFF";
+}
+
+function onGreenContentClick( )
+{
+	focusContent.style.backgroundColor = "#3FFF00";
+}
+
+function onYellowContentClick( )
+{
+	focusContent.style.backgroundColor = "#FFFF00";
+}
+
+function onRedContentClick( )
+{
+	focusContent.style.backgroundColor = "#FF0000";
+}
+
+function updateContentDivSize( )
+{
+	focusContent = document.getElementById( "focusContent" );
+	
+	//focusContent.style.backgroundColor = "#FF0000";
+
+	if( window.innerWidth > maxWidth )
+		focusContent.style.width = maxWidth + "px";
+	else
+		focusContent.style.width = ( window.innerWidth - 20 ) + "px";
+	
+	if ( window.innerHeight > maxHeight )
+		focusContent.style.height = maxHeight + "px";
+	else
+		focusContent.style.height = ( window.innerHeight - 20 ) + "px";
+}
+
+
+$(document).ready( onLoad );
+$(window).resize( onResize );
